4a644c222bbee6b12b28d89d3ace1454452769a1
Aaron Christiansen <aaronc20000@gmail.com>
2018-08-30 18:28:51 +0100

    Add new /api/games/:id endpoint

--- server.js
+++ server.js
@@ -34,6 +34,26 @@ app.get('/', (req, res) => {
     res.redirect("/index.html");
 });
 
+async function gameById(id) {
+    const target_uri =
+        `https://store.steampowered.com/api/appdetails?appids=${id}`
+    const result = await request.get(target_uri);
+    const json = JSON.parse(result);
+    const entries = getEntries(id);
+
+    const data = json[id].data;
+
+    return {
+        game_name: data.name,
+        game_id: data.steam_appid,
+        game_image:
+            `https://steamcdn-a.akamaihd.net/steam/apps/${data.steam_appid}/header.jpg`,
+        store_link:
+            `https://store.steampowered.com/app/${data.steam_appid}/`,
+        entries: entries
+    };
+}
+
 async function gameSearch(term) {
     const target_uri =
         `https://store.steampowered.com/api/storesearch/?term=${term}`
@@ -70,6 +90,11 @@ app.get('/api/games/search/:term/with_entries', (req, res) => {
         })))
     );
 });
+
+app.get('/api/games/:id', (req, res) => {
+    gameById(parseInt(req.params.id)).then(game =>
+        res.send(JSON.stringify(game)));
+})
 
 app.get('/api/games/:id/entries', (req, res) => {
     getEntries(req.params.id, rows => res.send(JSON.stringify(rows)));
