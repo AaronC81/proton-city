26a4463d0a7b92a0d83e30c5c9e9ee1b489778af
Aaron Christiansen <aaronc20000@gmail.com>
2018-09-01 08:56:10 +0100

    Create SteamUser class

---
+++ src/steam_user.ts
@@ -0,0 +1,31 @@
+import * as $ from "jquery";
+import * as Database from "./database";
+
+/**
+ * Represents a Steam user whose games can be retrieved.
+ */
+export class SteamUser {
+    /**
+     * Returns the authenticated user's info.
+     */
+    static async info() {
+        const userInfo = await $.getJSON("/api/user/info");
+
+        if (userInfo.errorMessage) {
+            return null;
+        }
+
+        return userInfo as {
+            steamid: string, username: string, profile: string,
+            avatar: { small: string, medium: string, large: string }
+        };
+    }
+
+    /**
+     * Gets the games owned by the authenticated user.
+     */
+    static async ownedGames(): Promise<Database.Game[]> {
+        const games = await $.getJSON("/api/user/owned");
+        return games.map((game: any) => new Database.Game(game));
+    }
+}
