d93ad8a1707e32adcb2c1b2a68d3cf7794de28b9
Aaron Christiansen <aaronc20000@gmail.com>
2018-09-01 09:25:09 +0100

    Actually export from UI

--- src/ui.tsx
+++ src/ui.tsx
@@ -4,4 +4,4 @@ import { GameSearch } from "./ui/game_search";
 import { GameSearchPage } from "./ui/game_search_page";
 import { UserGamesPage } from "./ui/user_games_page";
 
-export { Header, GameRow, GameSearch, GameSearchPage };
+export { Header, GameRow, GameSearch, GameSearchPage, UserGamesPage };
