b833333ed415f12c3d7a9e6db21001ea36d789dc
Aaron Christiansen <aaronc20000@gmail.com>
2018-09-02 17:12:41 +0100

    Compact navbar

--- src/ui/header.tsx
+++ src/ui/header.tsx
@@ -9,21 +9,29 @@ type HeaderState = {}
  */
 export class Header extends React.Component<HeaderProps, HeaderState> {
     render() {
-        return <div id="header" style={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-between"
+        return <div style={{
+                width: "100%",
+                backgroundImage: "url(/img/bg.png)",
+                backgroundSize: "cover",
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
             }}>
-            <div>
-                <h1>Proton City</h1>
-                <div id="nav">
-                    <a href="/">Search</a>
+            <div style={{ padding: "20px", display: "table" }}>
+                <span style={{
+                    fontSize: "1.5rem",
+                    color: "black",
+                    fontWeight: "bold",
+                    paddingRight: "20px"
+                }}>Proton City</span>
+                <span style={{ display: "table-cell", verticalAlign: "middle" }}>
+                    <a href="/">Home</a>
                     &nbsp;| <a href="/user_games.html"><b>NEW!</b> My Games</a> 
                     &nbsp;| <a href="https://addons.mozilla.org/en-GB/firefox/addon/proton-city/"><b>NEW!</b> Firefox Extension</a>
                     &nbsp;| <a href="https://github.com/OrangeFlash81/proton-city">GitHub</a>
-                </div>
+                </span>
             </div>
-            <div>
+            <div style={{ padding: "10px" }}>
                 <Account />
             </div>
         </div>
