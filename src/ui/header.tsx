881e06c82d1874d3c008860c06d4d0f20d965108
Aaron Christiansen <aaronc20000@gmail.com>
2018-09-03 19:08:06 +0100

    Fix very broken styling

--- src/ui/header.tsx
+++ src/ui/header.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
 import { Account } from "./account"
+import { Styles } from "./styles";
 
 type HeaderProps = {}
 type HeaderState = {}
@@ -15,19 +16,20 @@ export class Header extends React.Component<HeaderProps, HeaderState> {
                 backgroundSize: "cover",
                 display: "flex",
                 flexDirection: "row",
-                justifyContent: "space-between",
+                justifyContent: "space-between"
             }}>
             <div style={{ padding: "20px" }}>
                 <span style={{
                     color: "black",
                     fontWeight: "bold",
-                    paddingRight: "20px"
+                    paddingRight: "20px",
+                    ...Styles.navLink
                 }}>Proton City</span>
-                <span>
-                    <a href="/">Home</a>
-                    &nbsp;| <a href="/user_games.html"><b>NEW!</b> My Games</a> 
-                    &nbsp;| <a href="https://addons.mozilla.org/en-GB/firefox/addon/proton-city/"><b>NEW!</b> Firefox Extension</a>
-                    &nbsp;| <a href="https://github.com/OrangeFlash81/proton-city">GitHub</a>
+                <span style={ Styles.navLink }>
+                    <a href="/" style={ Styles.navLink }>Home</a>
+                    &nbsp;| <a href="/user_games.html" style={ Styles.navLink }><b>NEW!</b> My Games</a> 
+                    &nbsp;| <a href="https://addons.mozilla.org/en-GB/firefox/addon/proton-city/" style={ Styles.navLink }><b>NEW!</b> Firefox Extension</a>
+                    &nbsp;| <a href="https://github.com/OrangeFlash81/proton-city" style={ Styles.navLink }>GitHub</a>
                 </span>
             </div>
             <div style={{ padding: "20px" }}>
